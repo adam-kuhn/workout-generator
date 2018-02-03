1af414040010a54e6f6155f71a62679d7f6b10e3
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-03 17:37:06 +1300

    boiler plate set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,17 @@
+
+// Import
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div>
+      <Router>
+        <h1>React development has begun, for the Workout Generator!</h1>
+        {/* <Home /> */}
+    
+    </Router>
+    </div>
   )
 }
 
