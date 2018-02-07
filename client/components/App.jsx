f811496c197031ba78f8c263e2df003e219923f1
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-07 19:39:49 +1300

    basic workout page setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,6 +7,7 @@ import {HashRouter as Router, Route} from 'react-router-dom'
 import Home from './Home'
 import Time from './Time'
 import Gear from './Gear'
+import Workout from './Workout'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Route exact path='/' component={Home}/>
           <Route path='/time' component={Time}/>
           <Route path='/gear' component={Gear}/>
+          <Route path='/wod' component={Workout} />
         </div>
       </Router>
     </div>
---
+++ client/components/Workout.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Workout (props) {
+  // take props and display the workout
+  // have a button to display another/return to home
+  return (
+    <div className='container'>
+      <div className="header">
+        {/* this will be the title from props */}
+        <h1>Your Workout</h1>
+      </div>
+      <div>
+        <div className='flex-container'>
+          <div className="general-form">
+            <div className='form-body'>
+              {/* description will come from props */}
+              {/* will have to figure out how to display this nicely */}
+              <p>3x20 Reverse Lunge (weighted), 15 toes 2 air - E2MOM for 10 min: 1 min wall sit, 1 min plank - 4x 10 walking lunges (weighted), 10 burpees
+              </p>
+            </div>
+            {/* need to assing onClick=event handler to the button */}
+            {/* go through the array to produce another...if null/undefined go back to [0] */}
+            <button type='button'>Give me Another</button>
+            {/* need to assing onClick=event handler to the button */}
+            <button type='button'>See All</button>
+            {/* need to assing onClick=event handler to the button */}
+            <button type='button'>Start Over</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Workout
