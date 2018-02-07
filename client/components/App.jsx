698f1656372c6dd5cf57df0ac1145b7df6d8c4dd
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-07 18:45:35 +1300

    basic Duration setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,6 +5,7 @@ import {HashRouter as Router, Route} from 'react-router-dom'
 
 // components
 import Home from './Home'
+import Time from './Time'
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       <Router>
         <div className='container'>
           <h1>WOD Generator</h1>
-          <Route path='/' component={Home}/>
+          <Route exact path='/' component={Home}/>
+          <Route path='/time' component={Time}/>
         </div>
       </Router>
     </div>
---
+++ client/components/Time.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+class Time extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  render () {
+    return (
+      <div className='container'>
+        <div className="header">
+          <h1>Select Duration: </h1>
+        </div>
+        <div>
+          <div className='flex-container'>
+            <form className="home-form">
+              <div className='form-body'>
+                <label>0-20 min
+                  <input className='run-input' type='radio' name='time' value='0' />
+                </label>
+                <label>20-40 min
+                  <input className='run-input' type='radio' name='time' value='1' />
+                </label>
+                <label>40-60 min
+                  <input className='run-input' type='radio' name='time' value='3' />
+                </label>
+                <label>>60 min
+                  <input className='run-input' type='radio' name='time' value='4' />
+                </label>
+              </div>
+              {/* need to assing onClick=event handler to the button */}
+              <button type='button'>Next</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Time
