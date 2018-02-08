fbb80f9d54666fe2e4567b569f4ab54ec75bc8b1
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-08 16:05:46 +1300

    filling out the seeds with type and time

---
+++ migrations/20180208150837_type-and-time.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('workouts', table => {
+    table.string('type')
+    table.string('time')
+  })
+  
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('workouts', table => {
+    table.dropColumn('type')
+    table.dropColumn('time')
+  })
+};
