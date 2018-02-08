9d66240fb042e2faa2a9eeb208fcbd931af40615
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-08 18:12:48 +1300

    bb -> barbell and a few comments on gear seed

--- seeds/gear.js
+++ seeds/gear.js
@@ -6,13 +6,13 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('gear').insert([
         {id: 1, equipment: 'none'},
-        {id: 2, equipment: 'kb/db'},
+        {id: 2, equipment: 'kb/db'}, //kettle bell and dumbbell
         {id: 3, equipment: 'pull-up'},
-        {id: 4, equipment: 'bb'},
+        {id: 4, equipment: 'barbell'},
         {id: 5, equipment: 'box'},
-        {id: 6, equipment: 'sandbag'},
+        {id: 6, equipment: 'sandbag'}, //includes bucket
         {id: 7, equipment: 'rope'},
-        {id: 8, equipment: 'row'}
+        {id: 8, equipment: 'row'} // rowing maching
       ]);
     });
 };
