7cf2ddea37694b0e3d1a21801e943f71fbad9484
adam-kuhn <adam.kuhn20@gmail.com>
2018-02-11 18:49:14 +1300

    able to handle state of the form

--- client/components/Gear.jsx
+++ client/components/Gear.jsx
@@ -3,8 +3,35 @@ import React from 'react'
 class Gear extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      gearList: [],
+      none: null,
+      kb: null,
+      pullUp: null,
+      sandbag: null,
+      barbell: null,
+      box: null
+
+    }
+    this.setGearList = this.setGearList.bind(this)
   }
+  setGearList (evt) {
+    if (this.state.none === evt.target.value ||
+        this.state.kb === evt.target.value ||
+        this.state.pullUp === evt.target.value ||
+        this.state.sandbag === evt.target.value ||
+        this.state.barbell === evt.target.value ||
+        this.state.box === evt.target.value) {
+      this.setState({
+        [evt.target.value]: null
+      })
+    } else {
+      this.setState({
+        [evt.target.name]: evt.target.value
+      })
+    }
+  }
+
   render () {
     return (
       <div className='container'>
@@ -16,22 +43,22 @@ class Gear extends React.Component {
             <form className="general-form">
               <div className='form-body'>
                 <label className='gear'>None
-                <input className='general-input' type='checkbox' name='gear' value='0' />
+                <input className='general-input' type='checkbox' name='none' value='none' onChange={this.setGearList}/>
                 </label>
                 <label className='gear'>Kettle Bell/Dumbbell
-                <input className='general-input' type='checkbox' name='gear' value='1' />
+                <input className='general-input' type='checkbox' name='kb' value='kb' onChange={this.setGearList}/>
                 </label>
                 <label className='gear'>Pull-Up Bar
-                <input className='general-input' type='checkbox' name='gear' value='3' />
+                <input className='general-input' type='checkbox' name='pullUp' value='pullUp' onChange={this.setGearList}/>
                 </label>
                 <label className='gear'>Sandbag/Bucket
-                <input className='general-input' type='checkbox' name='gear' value='4' />
+                <input className='general-input' type='checkbox' name='sandbag' value='sandbag' onChange={this.setGearList}/>
                 </label>
                 <label className='gear'>Barbell
-                <input className='general-input' type='checkbox' name='gear' value='4' />
+                <input className='general-input' type='checkbox' name='barbell' value='barbell' onChange={this.setGearList}/>
                 </label>
                 <label className='gear'>Box
-                <input className='general-input' type='checkbox' name='gear' value='4' />
+                <input className='general-input' type='checkbox' name='box' value='box' onChange={this.setGearList}/>
                 </label>
               </div>
               {/* need to assing onClick=event handler to the button */}
