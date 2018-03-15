5ef459db1c1a822c32ff0011615c734e6308e6c0
adam-kuhn <adam.kuhn20@gmail.com>
2018-03-16 12:10:21 +1300

    refined comments

--- server/db.js
+++ server/db.js
@@ -18,7 +18,7 @@ function getMulti (wodSelection, testDb) {
 
   const nonMatchingEquipment = db('gear')
     .whereNotIn('equipment', selectedGear).select('equipment')
-
+  // returns instances that have one of the selected items
   return db('workouts')
     .join('workout_gear', 'workouts.id', 'workout_gear.workout_id')
     .join('gear', 'workout_gear.gear_id', 'gear.id')
@@ -36,33 +36,28 @@ function getMulti (wodSelection, testDb) {
         const workoutEquipmentAmount = workoutNames.filter(name => {
           return name === workoutNames[i]
         })
-        // if the workout name doesn't appear the same amount of times as the gear selected, then you know it is doesn't fill the required selection
-        // compare length of the filtered Name array, to the amount of gear selected
+        // if workout instances don't match the amount of gear -> by comparing lengths
+        // then it doesn't have all the needed items
         if (workoutEquipmentAmount.length === gearAmount) {
           hasEquipmentAmount.push(workoutEquipmentAmount)
         }
       }
-      // need to double check workouts now and make sure that gear that wasn't selected isn't passing through
-      // get all gear items from the workouts provided
-      // try running another query, to see how what other equipment is used in the workout - atm when str, 0-20, kb and pu, selected we get chipper, but chipper alos needs a box
-      // can try to run another query to see all gear of the given workout, and test against what we already have
-      // search for workouts that have additional items then the selected gear, and remove them from our first query
-      // seems hacky but maybe hardcore some form of an array to test against? 
-      // could have an array of all the equipment, then filter out the equipment that isn't selected to create a new array of all unselected gear, then quary all workouts that contain that gear, and remove from the result?...potentially just going round in circle ...
-      // query on selecting all gear that is non matching equip, as in the sub query above: nonMatchingEquipment
-      
+      // get the names to run another query
+      // it is possible for some of these workouts to have extra gear items
       const namesWithEquipment = []
       for (i = 0; i < hasEquipmentAmount.length; i++) {
         namesWithEquipment.push(hasEquipmentAmount[i][0])
       }
     // remove dupes with a set
     const rmvDupe = new Set(namesWithEquipment)
-    const reducedNames = [...rmvDupe]
-
+    const namesForFinalQuery = [...rmvDupe]
+      // return will show all required gear
+      // if number of iterations is again not = gearAmount -> length
+      // then there is extra gear in the workout that was not selected
       return db('workouts')
       .join('workout_gear', 'workouts.id', 'workout_gear.workout_id')
       .join('gear', 'workout_gear.gear_id', 'gear.id')
-      .whereIn('workouts.workout', reducedNames)
+      .whereIn('workouts.workout', namesForFinalQuery)
       .select('workouts.workout', 'gear.equipment')
       .then(result => {
        const names = result.map(workout => {
@@ -90,11 +85,6 @@ function getMulti (wodSelection, testDb) {
        .select('workout', 'description')
       })
     })
-  
-    // if we do another queary on just the workout names, and see how many times they turn up now, then if it is greater then the gear list item then it will not fulfill the requirement
-    // above we take the case where if we only have 1 item appearing (line 65), current code should never return workouts more than the amount of gear selected.
-    // so if we run an original query to see all the gear for the already returned workouts, we will no be able to filter out any that are above the gear requirement and therefore all workouts that don't have the exact gear requirements will have been removed!!...i thinks
-
     .catch(err => {
       console.error(err)
     })
