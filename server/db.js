7a6add4ad48cd35fa985a2ca3f4179a7586be0a8
adam-kuhn <adam.kuhn20@gmail.com>
2018-03-16 09:22:51 +1300

    getMulti returns workouts only with specific gear

--- server/db.js
+++ server/db.js
@@ -22,24 +22,6 @@ function getMulti (wodSelection, testDb) {
   const db = testDb || connection
   const gearAmount = selectedGear.length
 
-  // return db('workouts')
-  // .join('workout_gear', 'workout_gear.gear_id', 'workout.id')
-  // .join('gear', 'workout_gear.gear_id', 'gear.id')
-  // .where('workout.time', selectedDuration)
-  // .andWhere('workout.type', selectedType)
-  // .select('workout', 'type', 'time',
-  //   db.raw('GROUP_CONCAT(gear.equipment AS equips)', ['workouts', 'gear', 'workout_gear']))
-  //   .where(db.raw(1))
-  // // .groupBy('gear.equipment as equips')
-  // .groupBy('workouts.workout')
-  // .havingIn('equips', selectedGear)
-  // .then(result => {
-  //   console.log(result)
-  // })
-  // const selectedEquipmentIds = [2, 3] //kb/db and pull-up
-  // const nonMatchingEquipmentIds = db('gear')
-  //   .whereNotIn('id', selectedEquipmentIds).select('id')
-
   const nonMatchingEquipment = db('gear')
     .whereNotIn('equipment', selectedGear).select('equipment')
 
@@ -75,12 +57,59 @@ function getMulti (wodSelection, testDb) {
       // can try to run another query to see all gear of the given workout, and test against what we already have
       // search for workouts that have additional items then the selected gear, and remove them from our first query
       // seems hacky but maybe hardcore some form of an array to test against? 
-      const allGear = workouts.map(workout => {
-        return workout.equipment
-      })
+      // could have an array of all the equipment, then filter out the equipment that isn't selected to create a new array of all unselected gear, then quary all workouts that contain that gear, and remove from the result?...potentially just going round in circle ...
+      // query on selecting all gear that is non matching equip, as in the sub query above: nonMatchingEquipment
       
-      console.log('all gear', allGear)
+      const namesWithEquipment = []
+      for (i = 0; i < hasEquipmentAmount.length; i++) {
+        namesWithEquipment.push(hasEquipmentAmount[i][0])
+      }
+      console.log('names for query', namesWithEquipment)
+    // remove dupes with a set
+    const rmvDupe = new Set(namesWithEquipment)
+    console.log(rmvDupe)
+    const reducedNames = [...rmvDupe]
+    console.log(reducedNames)
+
+      return db('workouts')
+      .join('workout_gear', 'workouts.id', 'workout_gear.workout_id')
+      .join('gear', 'workout_gear.gear_id', 'gear.id')
+      .whereIn('workouts.workout', reducedNames)
+      .select('workouts.workout', 'gear.equipment')
+      .then(result => {
+       const names = result.map(workout => {
+         return workout.workout
+       })
+       // referring to the length of the array that will be created inside the array
+       const itemsForWorkout = []
+       for (let i = 0; i < names.length; i ++ ) {
+         const numberOfItems = names.filter(name => {
+           return name === names[i]
+         })
+         itemsForWorkout.push(numberOfItems)
+       }
+       console.log('how much gear?', itemsForWorkout)
+       const finalWorkoutNames = []
+       for (let i = 0; i < itemsForWorkout.length; i ++) {
+         if (itemsForWorkout[i].length === gearAmount) {
+           finalWorkoutNames.push(itemsForWorkout[i][0])
+         }
+       }
+       console.log('final names', finalWorkoutNames)
+       // rmv dupes, can break a lot of this code into other functions
+       const finalRmvDupes = new Set(finalWorkoutNames)
+       const finalNames = [...finalRmvDupes]
+       console.log('no dupes', finalNames)
+       return db('workouts')
+       .whereIn('workouts.workout', finalNames)
+       .select('workout', 'description')
+      })
     })
+  
+    // if we do another queary on just the workout names, and see how many times they turn up now, then if it is greater then the gear list item then it will not fulfill the requirement
+    // above we take the case where if we only have 1 item appearing (line 65), current code should never return workouts more than the amount of gear selected.
+    // so if we run an original query to see all the gear for the already returned workouts, we will no be able to filter out any that are above the gear requirement and therefore all workouts that don't have the exact gear requirements will have been removed!!...i thinks
+
     .catch(err => {
       console.error(err)
     })
