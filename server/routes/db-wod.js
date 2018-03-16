52bbb45fd299bf23b59fbd0c3ee5a97ff3930f9f
adam-kuhn <adam.kuhn20@gmail.com>
2018-03-16 22:07:49 +1300

    rmv some comments

--- server/routes/db-wod.js
+++ server/routes/db-wod.js
@@ -5,7 +5,6 @@ const db = require('../db')
 
 router.post('/', (req, res) => {
 if (req.body.type === 'Running') {
-    // refactor the .then(result) to be a function as all are the same
     db.getRunningWorkout(req.body)
       .then(result => {
         res.status(200).send({result})
