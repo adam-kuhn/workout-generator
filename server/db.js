afccc3dcd2e4a58f995686cfcadc96230e0aacf3
adam-kuhn <adam.kuhn20@gmail.com>
2018-03-19 14:43:45 +1300

    refactor getMulti function

--- server/db.js
+++ server/db.js
@@ -71,56 +71,16 @@ function getMulti (wodSelection, testDb) {
     .andWhere('workouts.time', selectedDuration)
     .select('workouts.workout', 'gear.equipment', 'workouts.type', 'workouts.time')
     .then(workouts => {
-      const workoutNames = []
-      console.log(workouts)
-      for (let i = 0; i < workouts.length; i++) {
-        workoutNames.push(workouts[i].workout)
-      }
-      const hasEquipmentAmount = []
-      for (let i = 0; i < workoutNames.length; i++) {
-        const workoutEquipmentAmount = workoutNames.filter(name => {
-          return name === workoutNames[i]
-        })
-        // if workout instances don't match the amount of gear -> by comparing lengths
-        // then it doesn't have all the needed items
-        if (workoutEquipmentAmount.length === gearAmount) {
-          hasEquipmentAmount.push(workoutEquipmentAmount)
-        }
-      }
-      // get the names to run another query
-      // it is possible for some of these workouts to have extra gear items
-      const namesWithEquipment = []
-      for (let i = 0; i < hasEquipmentAmount.length; i++) {
-        namesWithEquipment.push(hasEquipmentAmount[i][0])
-      }
-      const namesForFinalQuery = removeDuplication(namesWithEquipment)
-      // return will show all required gear
-      // if number of iterations is again not = gearAmount -> length
-      // then there is extra gear in the workout that was not selected
+      const workoutsWithEquipment = howMuchGear(workouts, gearAmount)
+      const namesForFinalQuery = removeDuplication(workoutsWithEquipment)
       return db('workouts')
         .join('workout_gear', 'workouts.id', 'workout_gear.workout_id')
         .join('gear', 'workout_gear.gear_id', 'gear.id')
         .whereIn('workouts.workout', namesForFinalQuery)
         .select('workouts.workout', 'gear.equipment')
-        .then(result => {
-          const names = result.map(workout => {
-            return workout.workout
-          })
-          // referring to the length of the array that will be created inside the array
-          const itemsForWorkout = []
-          for (let i = 0; i < names.length; i++) {
-            const numberOfItems = names.filter(name => {
-              return name === names[i]
-            })
-            itemsForWorkout.push(numberOfItems)
-          }
-          const finalWorkoutNames = []
-          for (let i = 0; i < itemsForWorkout.length; i++) {
-            if (itemsForWorkout[i].length === gearAmount) {
-              finalWorkoutNames.push(itemsForWorkout[i][0])
-            }
-          }
-          const finalNames = removeDuplication(finalWorkoutNames)
+        .then(finalWorkouts => {
+          const hasSpecificGear = howMuchGear(finalWorkouts, gearAmount)
+          const finalNames = removeDuplication(hasSpecificGear)
           return db('workouts')
             .whereIn('workouts.workout', finalNames)
             .select('workout', 'description')
