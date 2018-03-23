9c496d5ebdd37dab03298787a3cc4ddef799761e
adam-kuhn <adam.kuhn20@gmail.com>
2018-03-23 16:32:41 +1300

    set up display reducer

---
+++ client/reducers/display.js
@@ -0,0 +1,18 @@
+// import action string
+
+const initialState = {
+  showHeader: false,
+  showHome: true,
+  showTime: false,
+  showGear: false,
+  showWorkout: false
+}
+
+function display (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default display
