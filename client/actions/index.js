bac5810ab75cd1b7f2fa5a1895c41fd004a0e116
adam-kuhn <adam.kuhn20@gmail.com>
2018-03-24 13:47:15 +1300

    make time action

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,16 @@
 export const SET_TYPE = 'SET_TYPE'
+export const SET_TIME = 'SET_TIME'
 
 export const setType = (wodType) => {
   return {
     type: SET_TYPE,
     variety: wodType
+  }
+}
+
+export const setTime = (duration) => {
+  return {
+    type: SET_TIME,
+    duration
   }
 }
