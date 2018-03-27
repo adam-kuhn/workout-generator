951b6e363e48a5a1e8749d0f83bf779b8b5af26f
adam-kuhn <adam.kuhn20@gmail.com>
2018-03-27 22:41:57 +1300

    lint app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,6 @@
-// Import modules
 import React from 'react'
 import {connect} from 'react-redux'
-// components
+
 import FormHeader from './FormHeader'
 import Home from './Home'
 import Time from './Time'
@@ -9,24 +8,6 @@ import Gear from './Gear'
 import Workout from './Workout'
 
 class App extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      workoutType: '',
-      duration: '',
-      gearList: []
-    }
-    this.backToGear = this.backToGear.bind(this)
-  }
-
-  backToGear () {
-    this.setState({
-      showWorkout: false,
-      showGear: true,
-      gearList: []
-    })
-  }
-
   render () {
     return (
       <div>
@@ -39,9 +20,7 @@ class App extends React.Component {
           </div>
           <div className='main-item'>
             {this.props.display.showHome && <Home />}
-
             {this.props.display.showTime && <Time />}
-
             {this.props.display.showGear && <Gear />}
             {this.props.display.showWorkout && <Workout />}
           </div>
