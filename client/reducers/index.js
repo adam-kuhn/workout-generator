32472b5cd69f1524115a32cf38b5b12b7380ef7f
adam-kuhn <adam.kuhn20@gmail.com>
2018-03-31 21:05:45 +1300

    move gear reducer to config file

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,7 +3,7 @@ import {combineReducers} from 'redux'
 import display from './display'
 import selection from './selection'
 import workouts from './workouts'
-import gear from './gear'
+import gear from './config/gear'
 
 export default combineReducers({
   display,
