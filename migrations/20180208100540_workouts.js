4131f774b031b681bbdb6cb9481733465b83ce7e
adam-kuhn <adam.kuhn20@gmail.com>
2018-04-09 18:05:37 +1200

    fix typo

--- migrations/20180208100540_workouts.js
+++ migrations/20180208100540_workouts.js
@@ -1,4 +1,3 @@
-
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('workouts', table => {
     table.increments('id').primary()
--- migrations/20180208181900_workout_gear.js
+++ migrations/20180208181900_workout_gear.js
@@ -1,12 +1,11 @@
-
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
   return knex.schema.createTable('workout_gear', table => {
     table.increments('id').primary()
-    table.integer('workout_id').references('workout.id')
+    table.integer('workout_id').references('workouts.id')
     table.integer('gear_id').references('gear.id')
   })
-};
+}
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('workout_gear')  
-};
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('workout_gear')
+}
