3385c2b08b043b53ed750a403759186b1b25d56e
adam-kuhn <adam.kuhn20@gmail.com>
2019-06-19 22:02:43 +0300

    update App render test

--- tests/client/app.test.js
+++ tests/client/app.test.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import {Provider} from 'react-redux'
 import Enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 import App from '../../client/components/App'
+import {store} from '../../client/index'
 
 Enzyme.configure({adapter: new Adapter()})
 
-test('<App />', () => {
-  const expected = 'WOD Generator<Home />'
-  const wrapper = shallow(<App />)
+test('<App /> renders without crashing', () => {
+  const expected = '<Connect(App) />'
+  const wrapper = shallow(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
   expect(wrapper.text()).toBe(expected)
 })
