3ab1f8eb0693980fc3c88479a5abb9922ce7ac82
adam-kuhn <adam.kuhn20@gmail.com>
2019-06-23 08:07:14 +0300

    display test passing for SET_TIME

--- tests/client/reducers/display.test.js
+++ tests/client/reducers/display.test.js
@@ -1,5 +1,5 @@
 import display from '../../../client/reducers/display'
-import {SET_TYPE} from '../../../client/actions/selection'
+import {SET_TYPE, SET_TIME} from '../../../client/actions/selection'
 
 const initialState = {
   showHeader: false,
@@ -21,7 +21,52 @@ test('on SET_TYPE showHeader and showTime are true, showHome is false', () => {
     showTime: true
   }
   const actual = display(initialState, action)
+
   expect(actual.showHeader).toBe(expected.showHeader)
   expect(actual.showHome).toBe(expected.showHome)
   expect(actual.showTime).toBe(expected.showTime)
+})
+
+test('on SET_TIME, showTime is set to false, and showGear to true when wodType is not "Running"', () => {
+  const appState = {
+    showTime: true,
+    showGear: false,
+    showWorkout: false
+  }
+  const action = {
+    type: SET_TIME,
+    wodType: 'strength'
+  }
+  const expected = {
+    showTime: false,
+    showGear: true,
+    showWorkout: false
+  }
+  const actual = display(appState, action)
+
+  expect(actual.showTime).toBe(expected.showTime)
+  expect(actual.showGear).toBe(expected.showGear)
+  expect(actual.showWorkout).toBe(expected.showWorkout)
+})
+
+test('on SET_TIME, showTime is set to false, and showWorkout to true when wodType is "Running"', () => {
+  const appState = {
+    showTime: true,
+    showGear: false,
+    showWorkout: false
+  }
+  const action = {
+    type: SET_TIME,
+    wodType: 'Running'
+  }
+  const expected = {
+    showTime: false,
+    showGear: false,
+    showWorkout: true
+  }
+  const actual = display(appState, action)
+
+  expect(actual.showTime).toBe(expected.showTime)
+  expect(actual.showGear).toBe(expected.showGear)
+  expect(actual.showWorkout).toBe(expected.showWorkout)
 })
